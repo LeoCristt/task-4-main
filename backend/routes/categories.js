c2cb351bad212d03bd2f568760ea2c6218765740
LeoCristt <SergeySav009@yandex.ru>
2025-03-09 14:18:18 +0300

    delete routes

--- backend/routes/categories.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const data = require("../data/products.json");
-
-router.get("/", (req, res) => {
-    res.json(data.categories);
-});
-
-module.exports = router;
